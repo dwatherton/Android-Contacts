79bdb023e94272d9c312c9c686c15bfa74f0c700
Dwatherton <Dwatherton@ualr.edu>
2019-10-26 12:01:13 -0500

    Initial Commit.

---
+++ app/src/main/java/com/ualr/androidcontacts/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ualr.androidcontacts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
